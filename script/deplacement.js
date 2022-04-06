9c84100a14995cd76befdc304a89d15b587d5794
anonems <93661372+anonems@users.noreply.github.com>
2022-04-06 13:16:10 +0200

    ok

--- script/deplacement.js
+++ script/deplacement.js
@@ -3,7 +3,7 @@ let myMap = document.getElementById('mapzone')
 
 
 window.addEventListener('load', () =>{
-    myPerso.style.backgroundImage = "url(/data/perso.png)"
+    myPerso.style.backgroundImage = "url(data/perso.png)"
     myPerso.style.backgroundSize = "50px"
     myPerso.style.width = "50px"
     myPerso.style.height = "50px"
